31ca37f73893bc189a69498f2b64d64a8da262b8
root <root@localhost.localdomain>
2018-12-14 05:18:20 -0500

    pascals

---
+++ pascals-triangle/src/pascals_triangle.c
@@ -0,0 +1,36 @@
+#include "pascals_triangle.h"
+
+void free_triangle(size_t ** mem_block, size_t n)
+{
+   for (size_t i = 0; i < n; i++)
+{
+      free(mem_block[i]);
+   }
+   free(mem_block);
+}
+
+size_t **create_triangle(int rows)
+{
+   if (rows == 0) {
+      size_t **temp = calloc(1, sizeof(size_t *));
+      temp[0] = calloc(1, sizeof(size_t));
+      return temp;
+   } else if (rows < 0) {
+      return NULL;
+   }
+   size_t **result = calloc(rows, sizeof(size_t *));
+   for (int i = 0; i < rows; i++) 
+{
+      result[i] = calloc(rows, sizeof(size_t));
+   }
+   result[0][0] = 1;
+   for (int i = 1; i < rows; ++i) 
+{
+      result[i][0] = 1;
+      for (int j = 1; j <= i; ++j) 
+{
+         result[i][j] = result[i - 1][j] + result[i - 1][j - 1];
+      }
+}
+   return result;
+}
